00858be77f32c3449e926a1603f7d1f7918ff2da
Madelene Campos <madelenecampos@gmail.com>
2015-07-13 17:28:07 -0500

    [Maddie] Added all files through the Creating A New Topic section of the tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
+
+
+ # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'topics#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -52,5 +59,4 @@ Rails.application.routes.draw do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
-  #   end
-end
+     end
